4b865ed467b4a9cec85d98c9a413afc685f02d08
Jiang Ye <iamyoukou@gmail.com>
2019-08-07 17:13:30 +0900

    update

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 CXX=llvm-g++
 COMPILE=-c -std=c++11 \
--I/usr/local/Cellar/opencv/4.1.0_2/include/opencv4
+-I/usr/local/Cellar/opencv/4.1.0_2/include/opencv4 \
+-I/usr/local/Cellar/eigen/3.3.7/include/eigen3
 LINK=-L/usr/local/Cellar/opencv/4.1.0_2/lib -lopencv_core -lopencv_highgui -lopencv_imgcodecs \
 -lopencv_imgproc
 
--- test.cpp
+++ test.cpp
@@ -2,13 +2,17 @@
 #include <vector>
 #include <glm/glm.hpp>
 #include <glm/gtx/compatibility.hpp>
+#include <glm/gtx/string_cast.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <Eigen/Core>
+#include <Eigen/SVD>
 
 using namespace glm;
 using namespace std;
 using namespace cv;
+using namespace Eigen;
 
 const Scalar iWHITE(255, 255, 255);
 const Scalar iRED(0, 0, 255);
@@ -79,4 +83,61 @@ void plot(vector<vec2> &pts, Scalar color) {
   waitKey(1);
 }
 
-int main(int argc, char const *argv[]) { return 0; }
+// svd(A) --> VLR
+// V, R: 对称行列
+// L: 对角行列
+void svd(mat2 A, mat2 &V, mat2 &L, mat2 &R) {
+  // glm::mat to Eigen::Matrix
+  // note: difference of row-major and column-major
+  // Eigen::Matrix(row, col) = glm::mat[col][row]
+  Matrix<float, 2, 2> m;
+  m(0, 0) = A[0][0];
+  m(0, 1) = A[1][0];
+  m(1, 0) = A[0][1];
+  m(1, 1) = A[1][1];
+
+  JacobiSVD<Matrix<float, 2, 2>> jacobiSvd(m, Eigen::ComputeFullU |
+                                                  Eigen::ComputeFullV);
+  Matrix<float, 2, 2> v, r;
+  v = jacobiSvd.matrixU();
+  r = jacobiSvd.matrixV();
+
+  Matrix<float, 2, 1> l;
+  l = jacobiSvd.singularValues();
+
+  // output to V
+  V[0][0] = v(0, 0);
+  V[1][0] = v(0, 1);
+  V[0][1] = v(1, 0);
+  V[1][1] = v(1, 1);
+
+  // output to L
+  L[0][0] = l(0);
+  L[1][1] = l(1);
+
+  // output to R
+  R[0][0] = r(0, 0);
+  R[1][0] = r(0, 1);
+  R[0][1] = r(1, 0);
+  R[1][1] = r(1, 1);
+}
+
+int main(int argc, char const *argv[]) {
+  // Matrix<float, 2, 2> m;
+  // m << 0, 2.0, 1.0, 0;
+  mat2 M;
+  M[0][0] = 0.f;
+  M[1][0] = 2.f;
+  M[0][1] = 1.f;
+  M[1][1] = 0.f;
+
+  mat2 V, L, R;
+
+  svd(M, V, L, R);
+
+  std::cout << glm::to_string(V) << '\n';
+  std::cout << glm::to_string(L) << '\n';
+  std::cout << glm::to_string(R) << '\n';
+
+  return 0;
+}
